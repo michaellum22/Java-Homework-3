c47b3a90c4e28cb2a8567b2b82e9b30b578da332
michaellum22 <michaellum@Michaels-MBP.home>
2015-09-29 13:56:26 -0400

    Homework 3

--- src/Queens.java
+++ src/Queens.java
@@ -1,8 +1,9 @@
-//I had help from websites: http://introcs.cs.princeton.edu/java/23recursion/Queens.java.html & http://smahesh.com/blog/2013/03/29/eight-queens-puzzle/
-// I didn't really understand how to do the backtracking that the websites used, so I was only able to print one solution with all the queens
-
+/*I had help from websites: http://introcs.cs.princeton.edu/java/23recursion/Queens.java.html & http://smahesh.com/blog/2013/03/29/eight-queens-puzzle/
+//Originally I tried using a multidimensional array, but I found out that it would not work with backtracking, so I switched to a single array to store 
+the columns instead. As you suggested in the feedback I got, I looked up a tutorial for backtracking and had help from https://www.youtube.com/watch?v=ZM7RGa8W2KA
 import java.util.Scanner;
-
+*/
+import java.util.Scanner;
 public class Queens {
     
     public static void main (String args[])
@@ -10,66 +11,61 @@ public class Queens {
     	System.out.println ("Enter number of Queens"); // Asks user for input
     	   
         Scanner input = new Scanner(System.in); 
-    	int N = input.nextInt();;
+    	int N = input.nextInt();
         
-        int [][] board = new int[N][N+2]; //Creates board for queens based on user input
-        solve(0, board, N); //Checks if a queen can be placed
-        for (int i = 0; i <N; i++)
-        System.out.print("___");
-        System.out.println();
-        for (int i = 0; i <N; i++){    // For loop for row
-            for (int j = 0; j< N; j++) // For loop for column
-                if(board[i][j]==1)
-                	System.out.print(" Q ");
-                else
-                	System.out.print(" - ");
-        
-        System.out.println();
+         //Creates board for queens based on user input
+    	int[] board = new int[N];
+        Search(board, 0, N);
         }
-        for (int i = 0; i <N; i++)
+     static  void	print(int [] board, int N) // Prints board with queens
+       {
+    	 for (int i = 0; i <N; i++)
+             System.out.print("___");
+             System.out.println();
+    		for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (board[i] == j) 
+                    	System.out.print(" Q ");
+                    else          
+                    	System.out.print(" - ");
+                }
+                System.out.println();
+            }  
+    	
+    	for (int i = 0; i <N; i++)
             System.out.print("___");
         System.out.println();
-        }
-    	
+       }
     
+     static void Search(int[] board, int n, int M) { // Searches board to place Queens. Int n is needed for base case
+         int N = M;
+         if (n == N) print(board, M); // Prints solution for 1 combination
+         else {
+             for (int i = 0; i < N; i++) {
+                 board[n] = i;
+                 if (Checkboard(board, n, 0) == 1) 
+                	Search(board, n+1, M); //Recursively calls the function until n becomes the number of queens
+             }
+         }
+     }  
+     
+     static int Checkboard(int[] board, int N, int i) { // Method checks board for locations.
+         
+         while((i < N))
+         {
+        	 if (board[i] == board[N]) // Checks if the queen is on the column
+             return 0;
+        	 else if ((board[N] - board[i]) == (N - i) || (board[i] - board[N]) == (N - i)) // checks if the queen is diagonal from the proposed location
+        	 return 0;
+            i++;
+         }
+         return 1;
+         }
+     
+
+
     
-    static boolean solve(int row, int[][] board, int N) // Method changes values in the array from 0 to 1 
-    {
-        if (row>=N) return true; // Returns to main
-        for (int position = 0; position < N; position++) // Checks the column on each row
-        {
-            if (isValid(board, row, position, N)) //Sees if it is possible to have a queen
-            {
-                board[row][position] = 1; // Sets a value for the main
-                if(!solve(row+1, board, N)) // Method checks recursively
-                
-                board[row][position] = 0;
-                
-                else
-                    return true;
-            }
-        }
-        return false;
-    }
-    
-    static boolean isValid(int[][] board, int row, int column, int N) // Method checks if space is valid
-    {
-        int i, j;
-        for (i = 0; i< row; i++) //Checks the row from the left side
-            if(board[i][column] == 1)
-        return false;
-        i = row-1;
-        j= column-1;
-        while((i>=0)&&(j>=0)) //Checks diagonally on the upper left side
-        if(board[i--][j--]==1)
-        return false;
-        i = row-1;
-        j= column+1;
-        while((i >=0)&&(j<N)) // Checks diagonally on the lower left side
-        if(board[i--][j++]==1)
-        return false;
-        return true;
-        
-        
-    }
+
+
+   
 }
